e69c351af7f0dc95edc0e0fe9f83771604d43c11
dawniris <deversz@renewfund.com>
2015-05-29 10:52:50 -0700

    missing end bracket (again) - not the issue!

--- tetris.js
+++ tetris.js
@@ -1,10 +1,9 @@
-//Declare global object 'tetris'
 var tetris = {};
 
-//Draw gameplay grid
+//Draw 10x22 grid
 tetris.drawPlayField = function(){
   for(var row=0;row<22;row++){
-    $('#playfield').append('<tr class="'+row+'"</tr>');
+    $('#playfield').append('<tr class="'+row+'"></tr>');
     for(var col=0;col<10;col++){
       $('.'+row).append('<td id='"+col+"'></td>');
     }
