93bc15a76927b2c7904966190b26948d2caaaa2c
dawniris <deversz@renewfund.com>
2015-05-29 11:03:45 -0700

    the order of quotation marks matters YOU DICKWEED

--- tetris.js
+++ tetris.js
@@ -5,10 +5,13 @@ tetris.drawPlayField = function(){
   for(var row=0;row<22;row++){
     $('#playfield').append('<tr class="'+row+'"></tr>');
     for(var col=0;col<10;col++){
-      $('.'+row).append('<td id='"+col+"'></td>');
+      $('.'+row).append('<td id="'+col+'"></td>');
     }
   }
 }
+
+
+
 
 $(document).ready(function(){
   tetris.drawPlayField();
