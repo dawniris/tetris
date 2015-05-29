9695eddf9da66c7362057313d42a6f0c45fc760c
dawniris <deversz@renewfund.com>
2015-05-28 17:18:21 -0700

    added things that should have helped; did not

--- tetris.js
+++ tetris.js
@@ -4,9 +4,9 @@ var tetris = {};
 //Draw gameplay grid
 tetris.drawPlayField = function(){
   for(var row=0;row<22;row++){
-    $('#playfield').append('');
+    $('#playfield').append('<tr class="'+row+'"</tr>');
     for(var col=0;col<10;col++){
-      $('.'+row).append('');
+      $('.'+row).append('<td id='"+col+"'></td>');
     }
   }
 }
