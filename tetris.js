0e7f15d76ebe2022a235177861a495d3176a385b
dawniris <deversz@renewfund.com>
2015-05-29 14:36:47 -0700

    implement navigation on coordinate plane

--- tetris.js
+++ tetris.js
@@ -10,9 +10,63 @@ tetris.drawPlayField = function(){
   }
 }
 
+//need a variable to store current coordinates
+tetris.currentCoor = [{row:1,col:1},
+                      {row:1,col:2},
+                      {row:2,col:1},
+                      {row:2,col:2}];
+
+//fill cells according to specified coordinates
+tetris.fillCells = function(coordinates, fillColor){
+  for(var i=0;i<coordinates.length;i++){
+    var row = coordinates[i].row;
+    var col = coordinates[i].col;
+    var $coor = $('.'+row).find('#'+col);
+    $coor.attr('bgcolor',fillColor);
+    }
+}
+
+//moves by changing currentCoor based on directional input
+tetris.move = function(direction){
+  var reverse = false
+  this.fillCells(this.currentCoor,'');
+
+  for(var i=0;i<this.currentCoor.length;i++){
+    if(direction === 'right'){
+      this.currentCoor[i].col++;
+      if(this.currentCoor[i].col>9){
+        reverse = true
+      }
+    } else if (direction === 'left'){
+      this.currentCoor[i].col--;
+      if(this.currentCoor[i].col<0){
+        reverse = true
+      }
+    }
+  }
+
+  this.fillCells(this.currentCoor,'yellow');
+
+  if(reverse && direction === 'left'){
+    this.move('right');
+  } else if (reverse && direction === 'right'){
+    this.move('left');
+  }
+}
 
 
 
 $(document).ready(function(){
   tetris.drawPlayField();
+  tetris.fillCells(tetris.currentCoor, 'yellow')
 })
+
+$(document).keydown(function(m){
+  console.log(m.keyCode);
+  if(m.keyCode === 39){
+    tetris.move('right')
+  } else if (m.keyCode === 37){
+    tetris.move('left')
+  }
+})
+
